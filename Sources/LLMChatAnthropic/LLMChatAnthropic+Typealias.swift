3b8f9398e059ba321f55a314dab39a4ab48c32d1
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2024-10-27 06:43:19 +0700

    feat: adds type aliases (#5)

---
+++ Sources/LLMChatAnthropic/LLMChatAnthropic+Typealias.swift
@@ -0,0 +1,15 @@
+//
+//  LLMChatAnthropic+Typealias.swift
+//  LLMChatAnthropic
+//
+//  Created by Kevin Hermawan on 10/27/24.
+//
+
+import Foundation
+
+public extension LLMChatAnthropic {
+    typealias Completion = ChatCompletion
+    typealias CompletionChunk = ChatCompletionChunk
+    typealias Message = ChatMessage
+    typealias Options = ChatOptions
+}
